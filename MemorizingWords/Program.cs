fed1316032ff625be4c35a5d769103631710ce24
uniton <lovrovalx@gmail.com>
2020-09-21 01:32:57 +0300

    init

---
+++ MemorizingWords/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MemorizingWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
