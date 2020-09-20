397740b3b6037c746299cc433ea62317f058b030
uniton <lovrovalx@gmail.com>
2020-09-21 01:49:58 +0300

    added db context

---
+++ MemorizingWords/MemorizingWords.DAL/MemorizingWordsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MemorizingWords.MemorizingWords.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MemorizingWords.MemorizingWords.DAL
+{
+    public class MemorizingWordsDbContext : DbContext
+        {
+            public DbSet<Word> Words { get; set; }
+        
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Server=localhost;Database=WordsDB;Trusted_Connection=True;");
+            }
+        }
+}
