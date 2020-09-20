27dff7a30eca29e88d6bb54361fb85766171d1f0
uniton <lovrovalx@gmail.com>
2020-09-21 01:43:46 +0300

    added models

---
+++ MemorizingWords/MemorizingWords.Domain/Models/Word.cs
@@ -0,0 +1,9 @@
+﻿namespace MemorizingWords.MemorizingWords.Domain.Models
+{
+    public class Word
+    {
+        public int Id { get; set; }
+        public string OriginalWord { get; set; }
+        public string Translate { get; set; }
+    }
+}
