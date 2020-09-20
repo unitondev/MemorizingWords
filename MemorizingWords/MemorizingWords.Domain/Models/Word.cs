53f92b39ea4f4e7909ab2e870335e79a10af247d
uniton <lovrovalx@gmail.com>
2020-09-21 01:56:36 +0300

    added migrations

--- MemorizingWords/MemorizingWords.Domain/Models/Word.cs
+++ MemorizingWords/MemorizingWords.Domain/Models/Word.cs
@@ -1,9 +1,14 @@
-﻿namespace MemorizingWords.MemorizingWords.Domain.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MemorizingWords.MemorizingWords.Domain.Models
 {
     public class Word
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string OriginalWord { get; set; }
+        [Required]
         public string Translate { get; set; }
     }
 }
---
+++ MemorizingWords/Migrations/20200920225356_CreateWordsDB.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MemorizingWords.Migrations
+{
+    public partial class CreateWordsDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Words",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OriginalWord = table.Column<string>(nullable: false),
+                    Translate = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Words", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Words");
+        }
+    }
+}
