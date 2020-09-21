8916315f7d6cd05fc7a43d2b0fd6b67899146b3f
uniton <lovrovalx@gmail.com>
2020-09-22 02:10:23 +0300

    added directory BL with LogicInterface and implementation

---
+++ MemorizingWords/MemorizingWords.BL/Interfaces/IMemoLogic.cs
@@ -0,0 +1,16 @@
+﻿using MemorizingWords.MemorizingWords.DAL;
+using MemorizingWords.MemorizingWords.Domain.Models;
+
+namespace MemorizingWords.MemorizingWords.BL.Interfaces
+{
+    
+    public interface IMemoLogic
+    {
+        public void Initialize();
+        public bool CheckState();
+        public void StopState();
+        public Word ConsoleOutput(MemorizingWordsDbContext dbContext);
+        public void ConsoleInput(Word word);
+        public int GetLastIdInDb();
+    }
+}
--- MemorizingWords/Program.cs
+++ MemorizingWords/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using MemorizingWords.MemorizingWords.BL;
 using MemorizingWords.MemorizingWords.DAL;
 using MemorizingWords.MemorizingWords.Domain.Models;
 using Microsoft.Data.SqlClient;
@@ -10,55 +11,17 @@ namespace MemorizingWords
     {
         static void Main(string[] args)
         {
-            bool state = true;
-            var context = new MemorizingWordsDbContext();
-            Random random = new Random();
-            Console.WriteLine("Enter \"t\" for see translate, \"x\" to exit, any button for continue\n\n");
-
-            while (state)
+            MemoLogic repository = new MemoLogic();
+            MemorizingWordsDbContext context = new MemorizingWordsDbContext();
+            
+            repository.Initialize();
+            
+            while (repository.CheckState())
             {
-                int randomId = random.Next(1, GetLastId() + 1);
-                var word = context.Words.FirstOrDefault(word => word.Id == randomId);
-                Console.WriteLine(word.Translate);
+                Word word = repository.ConsoleOutput(context);
                 
-                CheckPressedButton(ref state, word);
+                repository.ConsoleInput(word);
             }
         }
-
-        static void CheckPressedButton(ref bool state, Word word)
-        {
-            var key = Console.ReadKey(true);
-            if (key.KeyChar == 't')
-            {
-                Console.WriteLine($"Translate - {word.OriginalWord}\n\n");
-            }
-            else if (key.KeyChar == 'x')
-            {
-                state = false;
-            }
-            else Console.WriteLine("\n");
-        }
-        static int GetLastId()
-        {
-            int lastId = 0;
-            string query = "SELECT IDENT_CURRENT('Words') AS Count";
-            string connectionString = @"Server=localhost;Database=WordsDB;Trusted_Connection=True";
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                SqlCommand command = new SqlCommand(query, connection);
-                connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-                
-                while (reader.Read())
-                {
-                    lastId = (int) (decimal) reader[0];
-                }
-                
-                reader.Close();
-                connection.Close();
-            }
-            return lastId;
-        }
-    }
+    } 
 }
